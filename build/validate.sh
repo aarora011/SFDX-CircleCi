127f6b6f3ccab64885ffd448ed05ec5eec5e74a1
anish <anish.arora99@gmail.com>
2021-12-07 16:51:26 +0530

    changes

--- build/validate.sh
+++ build/validate.sh
@@ -1,6 +1,7 @@
 if [ -s result.txt ]
 then
-    sfdx force:source:deploy -l RunSpecifiedTests -r Test -c -u testorg -x package/package.xml -w 30 --verbose --loglevel fatal
+    sfdx force:source:deploy -c -u testorg -x package/package.xml -w 30
+    sfdx force:apex:test:run -n TaskUtilTest -u testorg -r human
 else
     sfdx force:source:deploy -c -u testorg -x package/package.xml -w 30
 fi
